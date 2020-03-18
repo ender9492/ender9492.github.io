35fb16e05d6c84b7537b0b73e65fbb7172a5cee3
ender9492 <ender9492@users.noreply.github.com>
2020-03-18 12:24:51 -0700

    update

--- evrmor/js/scripts.js
+++ evrmor/js/scripts.js
@@ -1,36 +1,20 @@
 // JUMP CODE //
 
-// The function actually applying the offset
-function offsetAnchor() {
-	if (location.hash.length !== 0) {
-	  window.scrollTo(window.scrollX, window.scrollY - 100);
-	}
-  }
+// // The function actually applying the offset
+// function offsetAnchor() {
+// 	if (location.hash.length !== 0) {
+// 	  window.scrollTo(window.scrollX, window.scrollY - 100);
+// 	}
+//   }
   
-  // Captures click events of all <a> elements with href starting with #
-  $(document).on('click', 'a[href^="#"]', function(event) {
-	// Click events are captured before hashchanges. Timeout
-	// causes offsetAnchor to be called after the page jump.
-	window.setTimeout(function() {
-	  offsetAnchor();
-	}, 0);
-  });
+//   // Captures click events of all <a> elements with href starting with #
+//   $(document).on('click', 'a[href^="#"]', function(event) {
+// 	// Click events are captured before hashchanges. Timeout
+// 	// causes offsetAnchor to be called after the page jump.
+// 	window.setTimeout(function() {
+// 	  offsetAnchor();
+// 	}, 0);
+//   });
   
-  // Set the offset when entering page with hash present in the url
-  window.setTimeout(offsetAnchor, 0);
-
-
-  // PARALLAX SCROLL //
-
-  const parllax = document.querySelector(".parallax");
-
-window.addEventListener("scroll", function() {
-	let offset = window.pageYOffset;
-	parllax.style.backgroundPositionY = offset * 0.5 + 'px';
-});
-
-
-  // AOS //
-  <script>
-  AOS.init();
-</script>
+//   // Set the offset when entering page with hash present in the url
+//   window.setTimeout(offsetAnchor, 0);
